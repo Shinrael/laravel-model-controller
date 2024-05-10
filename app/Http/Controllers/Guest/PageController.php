d711fc677e01f965448b707e9c29226c4e2c74b1
Shinrael <mircoelena89@gmail.com>
2024-05-10 16:55:40 +0200

    First Commit

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Movie;
+
+class PageController extends Controller
+{
+    public function index(){
+
+        $movies = Movie::all();
+
+        return view('home', compact('movies'));
+    }
+
+    public function nuovaPagina(){
+        return view('nuova-pagina');
+    }
+
+}
